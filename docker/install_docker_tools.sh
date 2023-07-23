b816022516406391196c0f7c1053be4aa6d4d069
deliadam99 <adam.deli199@gmail.com>
2023-07-23 15:07:55 +0200

    Appended the script file.

--- docker/install_docker_tools.sh
+++ docker/install_docker_tools.sh
@@ -4,6 +4,7 @@ if ! command -v docker &> /dev/null; then
         echo "Failed to install Docker. Aborting."
         exit 1
     fi
+	echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo usermod -aG docker $USER
     echo "Docker installed successfully."
 else
